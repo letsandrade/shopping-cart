b3850adf394479d50fcedb2edd286a26456aaec6
leticiaandrade <leticia933@gmail.com>
2022-01-24 03:57:27 -0300

    requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (productId) => {
+  const productUrl = `https://api.mercadolibre.com/items/${productId}`;
+
+  try {
+    const response = await fetch(productUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
