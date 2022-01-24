7ea42198ba9378d9d0a4c20deac76cf2fcd90724
leticiaandrade <leticia933@gmail.com>
2022-01-24 03:18:46 -0300

    req 8

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -21,5 +21,5 @@ describe('1 - Teste a função fecthProducts', () => {
   it("Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.", async () => {
     const resultNoParam = await fetchProducts();
     expect(resultNoParam).toEqual(new Error('You must provide an url'))
-  })
+  });
 });
